5aeca7c6610103f4c43b31525f99c211dc3881b8
mafro <github@mafro.net>
2015-02-25 12:01:13 +1100

    option to log all printer output

--- printer.py
+++ printer.py
@@ -20,6 +20,9 @@ class CliPrinter:
     ERROR = 'ERROR'
     DEBUG = 'DEBUG'
 
+    log_output = False
+    logs = []
+
     def __init__(self, start=None, debug=False, progressbar_len=PROGBAR_LEN, progressbar_char="#"):
         self.start = start
         self.debug = debug
@@ -78,6 +81,10 @@ class CliPrinter:
 
         if success is False:
             out = sys.stderr
+
+        # log all prints to a stack for later use
+        if self.log_output is True:
+            self.logs.append(u'[{: <10}]  {}'.format(prefix, msg))
 
         t = self._get_time_elapsed(notime)
         out.write('{}[{: <10}]{} {: >4} {}{}{}'.format(
